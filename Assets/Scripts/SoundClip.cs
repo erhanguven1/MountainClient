b07522ab7c5f0ae226d88f8698e4d54b844d8e30
Erhan <Erhan@DESKTOP-H1VOALU>
2021-03-29 11:21:18 +0300

    New shader for surface and surprise music

---
+++ Assets/Scripts/SoundClip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundClip : MonoBehaviour
+{
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio.time = 45.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
